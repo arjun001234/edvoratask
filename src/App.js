f40bfad4c217607315b43193f50a1385dcd45984
arjun001234 <arjunkanojia001@gmail.com>
2022-04-28 09:48:36 +0530

    =error class added

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   const {state} = React.useContext(AppContext)
 
   if (state[ERROR]){
-     return <div>
+     return <div className='error'>
        <h1>Something went wrong!</h1>
        <p>Reload Page</p>
      </div>
